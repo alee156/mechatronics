ca8465bba880da269de707aecd508b8c1aae9484
alee156 <lealbertle@gmail.com>
2017-12-01 03:19:15 -0500

    Create milestone2.ino

---
+++ milestone2.ino
@@ -0,0 +1,38 @@
+// Include the Servo library
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include <Servo.h>
+// Declare the Servo pin
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();Adafruit_DCMotor *myMotor = AFMS.getMotor(1);Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+int servoPin = 3;
+// Create a servo object
+Servo Servo1;
+
+void setup() {
+ Serial.begin(9600);           // set up Serial library at 9600 bps
+ AFMS.begin();
+ // We need to attach the servo to the used pin number
+ myMotor->setSpeed(50);
+ myMotor->run(FORWARD);  myMotor2->setSpeed(50);
+ myMotor2-> run(FORWARD);
+ // turn on motor
+ myMotor->run(RELEASE);
+ myMotor2->run(RELEASE);
+ Servo1.attach(servoPin);
+}
+ void loop(){
+   delay(5000);      myMotor->run(BACKWARD);
+   myMotor->setSpeed(166);
+   //167 goes left
+   myMotor2->run(FORWARD);
+   myMotor2->setSpeed(255);
+   delay(700);
+   myMotor2->run(RELEASE);
+   delay(1000);
+   myMotor2->run(FORWARD);
+   myMotor2->setSpeed(255);
+   delay(400);
+   myMotor2->run(RELEASE);
+   myMotor->run(RELEASE);
+   delay(50000);
+}
