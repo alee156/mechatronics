b052962d17707a1e6320638085f7e57d12eb89cf
alee156 <lealbertle@gmail.com>
2017-12-14 01:53:35 -0500

    Add files via upload

---
+++ finalfinalwhitegirlgotback/ATTACK.ino
@@ -0,0 +1,47 @@
+// Include the Servo library
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+// Declare the Servo pin
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+
+void setup() {
+ Serial.begin(9600);           // set up Serial library at 9600 bps
+ AFMS.begin();
+ // We need to attach the servo to the used pin number
+ myMotor->setSpeed(50);
+ myMotor->run(FORWARD);  
+ myMotor2->setSpeed(50);
+ myMotor2-> run(FORWARD);
+ // turn on motor
+ myMotor->run(RELEASE);
+ myMotor2->run(RELEASE);
+
+}
+ void loop(){
+ myMotor->setSpeed(200);
+ myMotor2->setSpeed(200);
+ myMotor->run(FORWARD);
+ myMotor2->run(FORWARD);
+
+ delay(200);
+
+ myMotor2->run(RELEASE);
+
+ delay(2000);
+
+ myMotor2->run(FORWARD);
+
+ //delay(500);
+ 
+ myMotor->run(RELEASE);
+
+ delay(2000);
+
+ myMotor2->run(RELEASE);
+
+ delay(5000000000000);
+}
+
+
---
+++ finalfinalwhitegirlgotback/DEFENSE.ino
@@ -0,0 +1,150 @@
+
+// Include the Servo library
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include <Servo.h>
+
+// Declare the Servo pin
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+int servoPin = 3;
+
+
+Servo Servo1;
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  AFMS.begin();
+  myMotor->setSpeed(90);
+  myMotor->run(FORWARD);
+  myMotor->run(RELEASE);
+  
+  myMotor2->setSpeed(75);
+  myMotor2-> run(FORWARD);
+  myMotor2->run(RELEASE);
+
+  Servo1.attach(servoPin);
+
+}
+
+void loop(){
+  
+  myMotor->setSpeed(100);
+  myMotor2->setSpeed(90);
+  myMotor->run(BACKWARD);
+  myMotor2->run(BACKWARD);
+  //Servo1.write(300);
+  delay(100);
+
+  Servo1.write(140);
+
+  delay(200);
+
+  Servo1.write(160);
+
+  delay(200);
+
+  Servo1.write(140);
+
+  delay(200);
+
+  Servo1.write(160);
+
+  delay(200);
+
+  Servo1.write(140);
+
+  delay(200);
+
+  Servo1.write(160);
+
+
+  /////////////////////////////
+  delay(200);
+  Servo1.write(140);
+
+  delay(200);
+
+
+
+  // Long long long
+
+  Servo1.write(140);
+
+  delay(1000);
+
+  Servo1.write(160);
+
+  delay(1500);
+
+  Servo1.write(140);
+
+  delay(1000);
+
+  Servo1.write(160);
+
+  delay(1500);
+
+  Servo1.write(140);
+
+  delay(1000);
+
+  Servo1.write(160);
+
+  delay(1500);
+  
+
+  // Short short short
+
+  Servo1.write(140);
+
+  delay(1000);
+
+  Servo1.write(160);
+
+  delay(200);
+
+  Servo1.write(140);
+
+  delay(200);
+
+  Servo1.write(160);
+
+  delay(200);
+
+  Servo1.write(140);
+
+  delay(200);
+
+  Servo1.write(160);
+
+ ///////
+
+   /////////////////////////////
+  delay(500);
+  Servo1.write(140);
+
+  //////////////////////////////
+
+  myMotor->run(FORWARD);
+  myMotor2->run(RELEASE);
+
+  delay(2500);
+
+  myMotor2->run(FORWARD);
+
+  delay(5000);
+
+  
+
+  ////////////////////////////////
+ 
+
+  myMotor2->setSpeed(0);
+  myMotor ->setSpeed(0); 
+  delay(20000000);
+
+}
